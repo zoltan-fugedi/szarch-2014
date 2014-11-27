20baa701850938604f191c653b594ec1cf500443
Zoltan Fugedi <kewin_8@freemail.hu>
2014-11-27 22:39:53 +0100

    rename debug -> output

---
+++ MedievalWarfare.WcfLib/Helper/OutputHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MedievalWarfare.Common;
+using MedievalWarfare.WcfLib.GameState;
+
+namespace MedievalWarfare.WcfLib.Debug
+{
+    public class OutputHelper
+    {
+
+        private GameStateController gsc;
+
+        public OutputHelper(GameStateController controller)
+        {
+            gsc = controller;
+        }
+
+        public string GetPlayerName(Player p)
+        {
+            if (p.PlayerId == gsc.PlayerOne.PlayerId)
+            {
+                return "PlayerOne";
+            }
+            else
+            {
+                return "PlayerTwo";
+            }
+        }
+
+    }
+}
